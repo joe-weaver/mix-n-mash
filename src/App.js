c3c8e63e750f188323351fa26641420e5a5f7309
Joe Weaver <jweaver@jahnelgroup.com>
2020-12-13 22:55:37 -0500

    improved styling of user page

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import { ProvideAuth } from "./utils/use-auth";
 import { ProvideToast } from "./utils/use-toast";
 import Splash from "./pages/SignIn/Splash.js";
 import SignIn from "./pages/SignIn/SignIn.js";
-// import Test from "./pages/Account/Account";
+// import Test from "./pages/UserPage/Test";
 
 const client = new ApolloClient({
   uri: "http://localhost:3000",
--- src/pages/UserPage/UserPage.js
+++ src/pages/UserPage/UserPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Button } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import { NavbarLinks } from "../../data/NavbarLinks";
 import MixtapeResultCard from "../../components/MixtapeResultCard/MixtapeResultCard";
 import { useMutation, useQuery } from "@apollo/client";
@@ -17,138 +17,111 @@ import { mixtapesClient, getUserPageMixtapes } from "../../services/mixtapesServ
 
 import { useAuth } from "../../utils/use-auth";
 
-import "../Page.css";
 import "./UserPageStyle.css";
 
-const UserPage  = (props) => {
-  let url = window.location.pathname.split("/");
-  let idFromUrl = url[url.length - 1];
-  const auth = useAuth();
-
-  let {loading, data} = useQuery(getUser, {variables: {id: idFromUrl}, client: userClient});
-  let mixtapeObj = {loading: null, error: null, data: null};
+export default function UserPage(){
+    let url = window.location.pathname.split("/");
+    let idFromUrl = url[url.length - 1];
+    const auth = useAuth();
   
-  mixtapeObj = useQuery(getUserPageMixtapes, {client: mixtapesClient, variables: {userId: auth.user._id, otherUserId: idFromUrl}});
+    let {loading, data} = useQuery(getUser, {variables: {id: idFromUrl}, client: userClient});
+    let mixtapeObj = {loading: null, error: null, data: null};
+    
+    mixtapeObj = useQuery(getUserPageMixtapes, {client: mixtapesClient, variables: {userId: auth.user._id, otherUserId: idFromUrl}});
+    
+    // Hook into notifications
+    const toaster = useToast();
   
-  // Hook into notifications
-  const toaster = useToast();
-
-  const [sendMashmateRequestMutation] = useMutation(sendMMRequestMut, {client: userClient, onCompleted: (data) => {
-    auth.getUser();
-  }});
-  const [followUserMutation]= useMutation(followMut, {client: userClient, onCompleted:() => auth.getUser()});
-  const [incrementUserFollowCountMutation]= useMutation(incFollowersCountMut, {client: userClient});
-  const [unfollowUserMutation]= useMutation(unfollowMut, {client: userClient, onCompleted:() => auth.getUser()});
-  const [decrementUserFollowCountMutation]= useMutation(decFollowersCountMut, {client: userClient});
-
-  const SendMashmateRequest = () => {
-    const mashmateRequest = {
-      senderId: auth.user._id,
-      username: auth.user.username,
-      seen: false
-    };
-
-    sendMashmateRequestMutation({variables: {
-      id: data.user._id, 
-      newMashmateRequest: mashmateRequest
+    const [sendMashmateRequestMutation] = useMutation(sendMMRequestMut, {client: userClient, onCompleted: (data) => {
+      auth.getUser();
     }});
-
-    // Notify user
-    toaster.notify("Request Sent", "You sent a mashmate request to " + data.user.username + ".");
-  }
-
-  const followUserFunct = () => {
-    if (auth.user._id === data.user._id){toaster.notify("You cannot follow yourself!")}
-    else if (auth.user.following.includes(data.user._id)){toaster.notify("You cannot follow this user again!")}
-    else{
-      console.log("Following User")
-      //setShowUnfollow(true);
-
-      followUserMutation({variables: {
-        id: auth.user._id, 
-        idToFollow: data.user._id
-      }});
-
-      incrementUserFollowCountMutation({variables: {
+    const [followUserMutation]= useMutation(followMut, {client: userClient, onCompleted:() => auth.getUser()});
+    const [incrementUserFollowCountMutation]= useMutation(incFollowersCountMut, {client: userClient});
+    const [unfollowUserMutation]= useMutation(unfollowMut, {client: userClient, onCompleted:() => auth.getUser()});
+    const [decrementUserFollowCountMutation]= useMutation(decFollowersCountMut, {client: userClient});
+  
+    const SendMashmateRequest = () => {
+      const mashmateRequest = {
+        senderId: auth.user._id,
+        username: auth.user.username,
+        seen: false
+      };
+  
+      sendMashmateRequestMutation({variables: {
         id: data.user._id, 
+        newMashmateRequest: mashmateRequest
       }});
+  
+      // Notify user
+      toaster.notify("Request Sent", "You sent a mashmate request to " + data.user.username + ".");
     }
-  }
-
-  const unfollowUserFunct = () => {
-    if (!(auth.user.following.includes(data.user._id))){toaster.notify("You cannot unfollow this user again!")}
-    else{
-      console.log("Unfollowing User")
-      //setShowUnfollow(false);
-
-      unfollowUserMutation({variables: {
-        id: auth.user._id, 
-        idToUnfollow: data.user._id
-      }});
-
-      decrementUserFollowCountMutation({variables: {
-        id: data.user._id, 
-      }});
+  
+    const followUserFunct = () => {
+      if (auth.user._id === data.user._id){toaster.notify("You cannot follow yourself!")}
+      else if (auth.user.following.includes(data.user._id)){toaster.notify("You cannot follow this user again!")}
+      else{
+        followUserMutation({variables: {
+          id: auth.user._id, 
+          idToFollow: data.user._id
+        }});
+  
+        incrementUserFollowCountMutation({variables: {
+          id: data.user._id, 
+        }});
+      }
     }
-  }
+  
+    const unfollowUserFunct = () => {
+      if (!(auth.user.following.includes(data.user._id))){toaster.notify("You cannot unfollow this user again!")}
+      else{  
+        unfollowUserMutation({variables: {
+          id: auth.user._id, 
+          idToUnfollow: data.user._id
+        }});
+  
+        decrementUserFollowCountMutation({variables: {
+          id: data.user._id, 
+        }});
+      }
+    }
 
-  return (
-    
-      
-    
-    <div>
-      <div className="page-container">
-        <Navbar currentPage={NavbarLinks} />
-        <Card className="page-content">
-          <Card.Header className="content-header">
-            <h1>{!loading && data.user.username}</h1>
-          </Card.Header>
-          <Card.Body>
-
-            {/*Bio stuff */}
-            <div className="user-top-body">
-              {!loading && data.user.bio}
-              <div className= "user-page-buttons">
-                {!loading && auth.user._id !== data.user._id &&
-                  <Button
-                    className="mm-btn-alt"
-                    onClick={SendMashmateRequest}
-                    disabled={loading || data.user.mashmates.reduce((acc, x) => (auth.user._id === x.id) || acc, false)
-                            || data.user.receivedMashmateRequests.reduce((acc, x) => (auth.user._id === x.senderId) || acc, false)}>
-                      {data.user.receivedMashmateRequests.reduce((acc, x) => (auth.user._id === x.senderId) || acc, false) && "Mashmate Request Sent"}
-                      {data.user.mashmates.reduce((acc, x) => (auth.user._id === x.id) || acc, false) && "Already mashmates"}
-                      {!data.user.receivedMashmateRequests.reduce((acc, x) => (auth.user._id === x.senderId) || acc, false) && !data.user.mashmates.reduce((acc, x) => (auth.user._id === x.id) || acc, false) && "Send Mashmate Request"}
-                    </Button>
-                  }
-                {!auth.user.following.includes(idFromUrl) ? 
-                (<Button className="mm-btn-alt"onClick={followUserFunct}>Follow</Button>) 
-                : 
-                (<Button className="mm-btn-alt"onClick={unfollowUserFunct}>Unfollow</Button>)
-                }
-              </div>
-            </div>
-
-            <div>
-              <h5>
-                Number of Followers: {!loading && data.user.numFollowers}
-              </h5>
-            </div>
-
-            {/*List of their public mixtapes  */}
-            <div className="users-mixtapes-container">
-              <h4>{!loading && data.user.username}'s Mixtapes</h4>
-                
-              <div className="scroll-content" style={{height: "90%"}}>
-              {!mixtapeObj.loading && mixtapeObj.data.getUserPageMixtapes.map((mixtape) => (
-              <MixtapeResultCard mixtape={mixtape} key={mixtape._id} />
-            ))}
-              </div>
-            </div>
-          </Card.Body>
-        </Card>
+    return (
+      <div className="mm-container scroll-screen">
+        <Navbar currentPage={NavbarLinks.ACCOUNT} />
+        <h1 className="page-title">Account</h1>
+        <div className="user-container">
+          <div className="username">{!loading && data.user.username}</div>
+          {!loading && auth.user._id !== data.user._id &&
+              <Button
+              className="mm-btn"
+              style={{marginTop: "2vh", marginLeft: "6vw"}}
+              onClick={SendMashmateRequest}
+              disabled={loading || data.user.mashmates.reduce((acc, x) => (auth.user._id === x.id) || acc, false)
+                      || data.user.receivedMashmateRequests.reduce((acc, x) => (auth.user._id === x.senderId) || acc, false)}>
+                  {data.user.receivedMashmateRequests.reduce((acc, x) => (auth.user._id === x.senderId) || acc, false) && "Mashmate Request Sent"}
+                  {data.user.mashmates.reduce((acc, x) => (auth.user._id === x.id) || acc, false) && "Already mashmates"}
+                  {!data.user.receivedMashmateRequests.reduce((acc, x) => (auth.user._id === x.senderId) || acc, false) && !data.user.mashmates.reduce((acc, x) => (auth.user._id === x.id) || acc, false) && "Send Mashmate Request"}
+              </Button>
+          }
+          <h4 className="followers">
+              {!loading && data.user.numFollowers} Follower{!loading && data.user.numFollowers !== 1 ? "s" : ""}
+              {!auth.user.following.includes(idFromUrl) ? 
+                  (<Button className="mm-btn" style={{marginLeft: "10vw"}} onClick={followUserFunct}>Follow</Button>) 
+                  : 
+                  (<Button className="mm-btn" style={{marginLeft: "10vw"}} onClick={unfollowUserFunct}>Unfollow</Button>)
+              }
+          </h4>
+          {/* BIO STUFF */}
+          <h3 style={{paddingLeft: "4vw", paddingTop: "4vh"}}>Bio:</h3>
+          <div className="user-bio-container">{!loading && data.user.bio}</div>
+          {/* MIXTAPES STUFF */}
+          <div className="users-mixtapes-container">
+              <h4 style={{paddingLeft: "2vw", paddingBottom: "2vw"}}>{!loading && data.user.username}'s Mixtapes</h4>
+              {!mixtapeObj.loading && mixtapeObj.data.getUserPageMixtapes.map((mixtape, index) => (
+                  <MixtapeResultCard mixtape={mixtape} index={index} key={mixtape._id} />
+              ))}
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
 }
-
-export default UserPage;
